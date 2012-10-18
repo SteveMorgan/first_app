2f2b081cf6ec9489272c4a8297cd935df1dbdcaa
Steve Morgan <=>
2012-10-18 09:39:37 +0100

    Add rails files to repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '4601f21318efa191262244af033f802288b5b52b897a80616fff518704a461c9de2abe5be899b3e45875ab9cbf234f825b749e5e6ba5cc2e5b744137b0f31700'
